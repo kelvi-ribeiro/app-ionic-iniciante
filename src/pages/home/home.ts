955ca3ea58032201a60510490de0e3964fbd558a
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-09-19 15:35:52 -0300

    Usando provider de filmes e listando na tela

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FilmeProvider } from '../../providers/filme/filme';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  filmes;
+  pathImagens = 'https://image.tmdb.org/t/p/w500/';
+  constructor(private navCtrl: NavController,
+              private filmeProvider:FilmeProvider) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  ionViewDidLoad(){
+    this.filmeProvider.listarUltimosFilmes()
+    .subscribe(filmes => {
+      this.filmes = filmes['results']
+      console.log(this.filmes[0])
+    })
   }
 
 }
